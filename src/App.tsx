ec5e7a6d62f6bb6242a56350de424a45111042d8
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-11-30 21:27:11 +0530

    created the react-ts project for practice and used less as css pre-processor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.less';
 
-function App() {
+
+interface robot {
+  name: string,
+  id: number,
+  email: string,
+}
+
+interface roboProps{
+  robots: Array<robot>,
+  searchField: string,
+};
+
+
+
+const App = () => {
+  const [robots, setRobots] = useState<roboProps["robots"] | []>([]);
+  const [searchField, setSearchField] = useState<roboProps["searchField"] | ''>('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => setRobots(users))
+      .catch(error => console.log(error))
+  }, []);
+
+  const onSearchChange = (event: React.SyntheticEvent<HTMLInputElement>):void => {
+    const target = event.target as typeof event.target & {value: string};
+    setSearchField(target.value)
+  }
+
+  const filteredRobots = robots.filter(robot =>{
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>ROBOTS</h1>
       </header>
+      <input type="text" value={searchField} onChange={onSearchChange} />
+    
+    <div className="robots">
+      {
+        filteredRobots.map(robot => (
+          <div key={robot.id} className="robot">
+            <img src={`https://robohash.org/${robot.id}?200x200`} alt="robot"/>
+            <div>
+              <h2>{robot.name}</h2>
+              <p>{robot.email}</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
     </div>
   );
 }
